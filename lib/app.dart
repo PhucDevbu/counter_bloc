30864f39a3370b54396f0580ec87f523f3fed65b
Phuc Devbu <duongphuc73@gmail.com>
2022-07-05 18:01:07 +0700

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'counter/counter.dart';
+import 'counter/view/counter_page.dart';
+
+
+class CounterApp extends MaterialApp {
+
+  const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
+}
---
+++ lib/counter/view/counter_page.dart
@@ -0,0 +1,16 @@
+import 'package:counter_app/counter/cubit/counter_cubit.dart';
+import 'package:counter_app/counter/view/counter_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: CounterView(),
+    );
+  }
+}
